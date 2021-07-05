237a72f3e0cd2d9930a1f5f21d7fc443670689fc
이혜진 <4mikemendes@gmail.com>
2021-07-05 08:29:13 +0200

    sum

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //create two variables first card and second card
 //set teir value to random between 2-11
 //create a variable sum and set it to sum the two variables above
-const min = Math.ceil(2);
-const max = Math.floor(11);
-let firstCard = Math.random() * (min - max);
-let secondCard = Math.random() * (min - max);
+
+let firstCard = Math.floor((Math.random()*11) + 2);
+let secondCard = Math.floor((Math.random()*11) + 2);
+
+console.log(firstCard)
+console.log(secondCard)
+
+let sum = firstCard + secondCard
+console.log(sum)
