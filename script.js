e8edf50de344cdffc8c05d1a7528d50e9df9797f
이혜진 <4mikemendes@gmail.com>
2021-07-05 09:07:31 +0200

    if

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//check if person is over 21
+
+// let age = 101
+
+// if (age >= 21) {
+//   console.log("You can now enter the club!")
+// } else {
+//   console.log("You are a baby, comeback when you are older")
+// }
+
+//check if they are 100 years, this will alow them to play for free
+
+// if (age === 100) {
+//   console.log("You are welcome to play for free for one day!")
+// } else if (age < 100) {
+//   console.log("You are too young!")
+// } else {
+//   console.log("You alredy had your day!")
+// }
+
+
 //create two variables first card and second card
 //set teir value to random between 2-11
 //create a variable sum and set it to sum the two variables above
@@ -10,3 +31,11 @@ console.log(secondCard)
 
 let sum = firstCard + secondCard
 console.log(sum)
+
+if (sum < 21 ) {
+  console.log("Do you want another card?")
+} else if (sum === 21) {
+  console.log("You have won!")
+} else {
+  console.log("You have lost! Try again!")
+}
